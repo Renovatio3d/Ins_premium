4a74a1bcaa92e35a28c9f65b739f47b7ec80de81
Shallu Nah <renovatio3d@gmail.com>
2020-01-02 17:45:40 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+// AUTHOR: Shallu Nah
+// PROGRAM DESCRIPTION: The program calculate the company InsureU, Insurance Premium
+// Collision = 2% orig car price – 0.15% of the orig car price per year old
+// Comprehensive = 0.35% of original cost + $250, Liability = $290
+
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cctype>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
